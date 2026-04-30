49ccf6fee08fe51501f4c0d9a2d180d233c5978b
Sockbee <samsockbee@gmail.com>
2026-04-30 19:02:35 +0900

    feat: 가게 및 미션 관련 entity 설정

---
+++ src/main/java/com/example/umc10th/domain/entity/Mission.java
@@ -0,0 +1,34 @@
+package com.example.umc10th.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "missions")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(name = "complete_point", nullable = false)
+    private int completePoint;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<UserMission> userMissions = new ArrayList<>();
+}
