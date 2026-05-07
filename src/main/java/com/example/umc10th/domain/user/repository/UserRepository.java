50c71dbb35eef1e4cd18fdca5b6def4def4503ed
Sockbee <samsockbee@gmail.com>
2026-05-07 17:55:44 +0900

    feat: User 엔티티에 name 필드 추가 및 포인트 추가 메서드 구현, UserController에서 서비스 연결

---
+++ src/main/java/com/example/umc10th/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.umc10th.domain.user.repository;
+
+import com.example.umc10th.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
