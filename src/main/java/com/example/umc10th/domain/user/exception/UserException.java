0f45ee7a7711d9825fa7f47c70c7ca19ae567558
Sockbee <samsockbee@gmail.com>
2026-05-07 17:57:53 +0900

    feat: 유저 예외처리 구현

---
+++ src/main/java/com/example/umc10th/domain/user/exception/UserException.java
@@ -0,0 +1,10 @@
+package com.example.umc10th.domain.user.exception;
+
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.exception.ProjectException;
+
+public class UserException extends ProjectException {
+    public UserException(BaseErrorCode errorCode) {
+        super(errorCode);
+    }
+}
